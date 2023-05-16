748d41df8d67d66ebff203914239a21b946a7133
bachpx-vbee <bachpx.vbee@gmail.com>
2023-05-16 11:35:42 +0700

    Login display

--- src/router/appRoutes.js
+++ src/router/appRoutes.js
@@ -1,7 +1,4 @@
 import Login from "../pages/Login";
-import Register from "../pages/Register";
-import ForgotPassword from "../pages/ForgotPassword";
-import Home from "../pages/Home";
 
 import routes from "../constants/route";
 
@@ -9,27 +6,6 @@ const allRoutes = [
   {
     path: routes.LOGIN,
     component: Login,
-    exact: true,
-    restricted: true,
-    isPrivate: false,
-  },
-  {
-    path: routes.HOME,
-    component: Home,
-    exact: true,
-    restricted: true,
-    isPrivate: true,
-  },
-  {
-    path: routes.REGISTER,
-    component: Register,
-    exact: true,
-    restricted: true,
-    isPrivate: false,
-  },
-  {
-    path: routes.FORGOTPASSWORD,
-    component: ForgotPassword,
     exact: true,
     restricted: true,
     isPrivate: false,
