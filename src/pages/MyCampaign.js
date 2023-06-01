d1ce251b965211abe9a5cb3f743bbd9814b30e8c
ngoctt-vbee <ngoctt.vbee@gmail.com>
2023-06-01 09:32:29 +0700

    My Campaign display

---
+++ src/pages/MyCampaign.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import MyCampaignContainer from "../containers/MyCampaign"
+export default function MyCampaign() {
+    return (
+        <MyCampaignContainer />
+    )
+}
--- src/router/appRoutes.js
+++ src/router/appRoutes.js
@@ -1,23 +1,62 @@
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-
+import Home from "../pages/Home";
+import MyCampaign from "../pages/MyCampaign"
 import routes from "../constants/route";
+import CampaignDetail from "../components/CampaignDetail";
 
 const allRoutes = [
-  {
-    path: routes.LOGIN,
-    component: Login,
-    exact: true,
-    restricted: true,
-    isPrivate: false,
-  },
+  // {
+  //   path: routes.LOGIN,
+  //   component: Login,
+  //   exact: true,
+  //   restricted: true,
+  //   isPrivate: false,
+  // },
   {
     path: routes.REGISTER,
     component: Register,
+    component: Home,
     exact: true,
     restricted: true,
     isPrivate: false,
   },
+  {
+    path: routes.HOME,
+    component: Home,
+    exact: true,
+    restricted: true,
+    isPrivate: false,
+  },
+  // {
+  //   path: routes.REGISTER,
+  //   component: Register,
+  //   exact: true,
+  //   restricted: true,
+  //   isPrivate: false,
+  // },
+  {
+    path: routes.MYCAMPAIGN,
+    component: MyCampaign,
+    exact: true,
+    restricted: true,
+    isPrivate: false,
+  },
+  {
+    path: routes.CAMPAIGN_DETAIL,
+    component: CampaignDetail,
+    exact: true,
+    restricted: true,
+    isPrivate: false,
+  }
+  // {
+  //   path: routes.FORGOTPASSWORD,
+  //   component: ForgotPassword,
+  //   exact: true,
+  //   restricted: true,
+  //   isPrivate: false,
+  // },
+
 ];
 
 export default allRoutes;
