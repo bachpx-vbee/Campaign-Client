f13e55de0069a649e3503f9f9892c58185fae87a
ngoctt-vbee <ngoctt.vbee@gmail.com>
2023-06-08 10:45:11 +0700

    Navbar display

---
+++ src/components/Navbar/index.style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  .home-icon{
+    color: white;
+    font-size: 40px;
+  }
+  
+  .mycampaign-box{
+    flex-grow:1;
+    display: flex;
+  }
+  @media (max-width: 600px) {
+    
+  }
+  Button{
+    color: white;
+  }
+  
+`;
