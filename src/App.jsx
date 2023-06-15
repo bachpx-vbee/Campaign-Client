85c9fc1a80d03d024a6eb763e0e5ed696080cafa
ngoctt-vbee <ngoctt.vbee@gmail.com>
2023-06-15 09:12:36 +0700

    checkpoitn

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import React from "react";
-import AppRouter from "./router";
-import Navbar from "./components/Navbar";
-import { useEffect } from "react";
+import React from 'react';
+import AppRouter from './router';
+
 function App() {
-  return <>
-    <Navbar />
-    <AppRouter />
-  </>;
+  return <AppRouter />;
 }
 
 export default App;
