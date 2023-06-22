93dd62c273d455d74e1745e037e9b644e6dc531e
ngoctt-vbee <ngoctt.vbee@gmail.com>
2023-06-22 09:27:47 +0700

    Navbar Layout Private Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from "react";
 import AppRouter from "./router";
-import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <AppRouter />
-    </>
-  );
+  return <AppRouter />;
 }
 
 export default App;
