dc13fb18f087169aad45233360176e395d957863
sucrub <bachspham@gmail.com>
2023-07-06 10:32:33 +0700

    Admin page

--- src/containers/AdminMenu/index.jsx
+++ src/containers/AdminMenu/index.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import AdminMenuStyle from "./index.style";
+import DataTable from "../../components/DataTable";
+
+const admin = [
+  {
+    id: "13h1k2lh3kl",
+    email: "bachpx.vbee@gmail.com",
+    role: "server-admin",
+  },
+  {
+    id: "1h32k21h323k",
+    email: "bachspham@gmail.com",
+    role: "owner",
+  },
+  {
+    id: "l1k2j32l1k3",
+    email: "bachspham1@gmail.com",
+    role: "admin",
+  },
+];
+
+const columns = ["ID", "Email", "Phân quyền", "Hành động"];
 
 const AdminMenu = () => {
-  return <div>This is admin menu</div>;
+  return (
+    <AdminMenuStyle>
+      <div className="add-button">
+        <Button variant="contained">Thêm admin</Button>
+      </div>
+      <DataTable columns={columns} data={admin} deleteIcon />
+    </AdminMenuStyle>
+  );
 };
 
 export default AdminMenu;
---
+++ src/containers/AdminMenu/index.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default styled.div`
+  .add-button {
+    width: 96.3vw;
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 1%;
+  }
+  .table {
+    margin: 0 auto;
+    width: 95vw;
+  }
+`;
