daf834354c50342fb7c2070bd6765f3d06321a10
sucrub <bachspham@gmail.com>
2023-07-11 11:41:00 +0700

    Admin with drawer

--- src/containers/AdminMenu/index.jsx
+++ src/containers/AdminMenu/index.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import { Delete as DeleteIcon } from "@mui/icons-material";
+import AdminMenuStyle from "./index.style";
+import DataTable from "../../components/DataTable";
+import AdminDrawer from "./AdminDrawer";
+import DeleteAlert from "../../components/DeleteAlert";
+
+const admins = [
+  {
+    id: "13h1k2lh3kl",
+    email: "bachpx.vbee@gmail.com",
+    role: "server-admin",
+  },
+  {
+    id: "1h32k21h323k",
+    email: "bachspham@gmail.com",
+    role: "owner",
+  },
+  {
+    id: "l1k2j32l1k3",
+    email: "bachspham1@gmail.com",
+    role: "admin",
+  },
+];
+
+const columns = ["ID", "Email", "Phân quyền", "Hành động"];
 
 const AdminMenu = () => {
-  return <div>This is admin menu</div>;
+  const [adminDrawer, setAdminDrawer] = useState(false);
+  const [deleteAlert, setDeleteAlert] = useState(false);
+
+  const handleAddAdminDrawer = () => {
+    setAdminDrawer(true);
+  };
+
+  const handleCloseAdminDrawer = () => {
+    setAdminDrawer(false);
+  };
+
+  const handleOpenDeleteAlert = () => {
+    setDeleteAlert(true);
+  };
+
+  const handleCloseDeleteAlert = () => {
+    setDeleteAlert(false);
+  };
+
+  const ActionButton = ({ row }) => (
+    <DeleteIcon className="delete-icon icon" onClick={handleOpenDeleteAlert} />
+  );
+
+  const adminWithActions = admins.map((admin) => ({
+    ...admin,
+    actions: <ActionButton row={admin} />,
+  }));
+
+  return (
+    <AdminMenuStyle>
+      <DeleteAlert open={deleteAlert} onClose={handleCloseDeleteAlert} />
+      <AdminDrawer open={adminDrawer} onClose={handleCloseAdminDrawer} />
+      <div className="add-button">
+        <Button variant="contained" onClick={handleAddAdminDrawer}>
+          Thêm admin
+        </Button>
+      </div>
+      <DataTable columns={columns} data={adminWithActions} />
+    </AdminMenuStyle>
+  );
 };
 
 export default AdminMenu;
