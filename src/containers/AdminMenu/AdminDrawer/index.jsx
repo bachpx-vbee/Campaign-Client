c6f8d50410fce4da4dd120367f181f47f8a3583c
sucrub <bachspham@gmail.com>
2023-07-11 11:41:00 +0700

    Admin with drawer

--- src/containers/AdminMenu/AdminDrawer/index.jsx
+++ src/containers/AdminMenu/AdminDrawer/index.jsx
@@ -1,27 +1,25 @@
-import { Box, Drawer, TextField, Typography, Button } from "@mui/material";
+import { Box, TextField, Typography, Button } from "@mui/material";
 import AdminDrawerStyle from "./index.style";
 
 const AdminDrawer = (props) => {
   const { open, onClose } = props;
   return (
-    <AdminDrawerStyle>
-      <Drawer anchor="right" open={open} onClose={onClose}>
-        <Box className="drawer">
-          <Typography className="title" variant="h4" color="primary">
-            Add admin
-          </Typography>
-        </Box>
-        <TextField
-          required
-          id="email"
-          label="Email"
-          autoFocus
-          className="text-field"
-        />
-        <Button variant="contained" className="submit-button">
-          Submit
-        </Button>
-      </Drawer>
+    <AdminDrawerStyle anchor="right" open={open} onClose={onClose}>
+      <Box className="drawer">
+        <Typography className="title" variant="h4" color="primary">
+          Add admin
+        </Typography>
+      </Box>
+      <TextField
+        required
+        id="email"
+        label="Email"
+        autoFocus
+        className="text-field"
+      />
+      <Button variant="contained" className="submit-button">
+        Submit
+      </Button>
     </AdminDrawerStyle>
   );
 };
